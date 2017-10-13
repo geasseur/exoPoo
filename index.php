3d4ebabb2921e4fec62483d17be2045d2565ebee
geasseur <baptiste.ventura@neuf.fr>
2017-10-13 11:12:36 +0200

    arrivé au tp mini jeu, exo 3 fait, exo4 en cours

--- index.php
+++ index.php
@@ -16,89 +16,325 @@
     </head>
     <body>
       <?php
-      class Personnage{
-        private $_degats; // Les dégâts du personnage.
-        private $_experience; // L'expérience du personnage.
-        private $_force; // La force du personnage (plus elle est grande, plus l'attaque est puissante).
+//TP mini jeu combat
+      // class Personnage{
+      //   private $_id;
+      //   private $_nom;
+      //   private $_degats;
+      //
+      //   const MOI = 1;
+      //   const PERSONNAGE_MORT = 2;
+      //   const PERSONNAGE_FRAPPE = 3;
+      //
+      //   public function __construct(array $donnees){
+      //     $this->hydrate($donnees);
+      //   }
+      //
+      //   public function hydrate(array $donnees){
+      //     foreach ($donnees as $key => $value)
+      //     {
+      //       $method = 'set'.ucfirst($key);
+      //
+      //       if (method_exists($this, $method))
+      //       {
+      //         $this->$method($value);
+      //       }
+      //     }
+      //   }
+      //
+      //   public function id(){
+      //     return $this->_id;
+      //   }
+      //
+      //   public function degats(){
+      //     return $this->_degats;
+      //   }
+      //
+      //   public function nom(){
+      //     return $this->_nom;
+      //   }
+      //
+      //   public function setId($id){
+      //     $id = (int) $id;
+      //     if ($id>0) {
+      //       $this->_id = $id;
+      //     }
+      //   }
+      //
+      //   public function setDegats($degats){
+      //     $degats = (int) $degats;
+      //     if ($degats >= 0 and $degats <= 100) {
+      //       $this->_degats = $degats;
+      //     }
+      //   }
+      //
+      //   public function setNom($nom){
+      //     if (is_string($nom)) {
+      //       $this->_nom = $nom;
+      //     }
+      //   }
+      //   //fonction pour cibler un personnage à attaquer et lui indiquer qu'il a été frappé
+      //   public function frapper(Personnage $perso){
+      //     if ($perso->id() = $this->_id) {
+      //       return self::MOI;
+      //     }
+      //     return $perso->recevoirDegat();
+      //   }
+      //
+      //   //fonction donnant les points de degats au perso frappé
+      //   public function recevoirDegat(){
+      //     $this->_degats += 5;
+      //     if ($this->_degats >= 100) {
+      //       return self::PERSONNAGE_MORT;
+      //     }
+      //     return self::PERSONNAGE_FRAPPE;
+      //   }
+      // }
+      //
+      // class PersonnageManager{
+      //   private $_bdd;
+      //
+      //   public function __construct($bdd){
+      //     $this->setBdd($bdd);
+      //   }
+      //
+      //   public function setDb(PDO $db){
+      //     $this->_db = $db;
+      //   }
+      //
+      //   public function add(Personnage $perso){
+      //     $add = $this->prepare('SELECT id, nom, degats FROM PersonnagesTP');
+      //     $add->bindValue(':nom'->$perso->nom());
+      //     $add->execute();
+      //     // Préparation de la requête d'insertion.
+      //     // Assignation des valeurs pour le nom du personnage.
+      //     // Exécution de la requête.
+      //
+      //     // Hydratation du personnage passé en paramètre avec assignation de son identifiant et des dégâts initiaux (= 0).
+      //   }
+      //
+      //   public function count()
+      //   {
+      //     // Exécute une requête COUNT() et retourne le nombre de résultats retourné.
+      //   }
+      //
+      //   public function delete(Personnage $perso)
+      //   {
+      //     // Exécute une requête de type DELETE.
+      //   }
+      //
+      //   public function exists($info)
+      //   {
+      //     // Si le paramètre est un entier, c'est qu'on a fourni un identifiant.
+      //       // On exécute alors une requête COUNT() avec une clause WHERE, et on retourne un boolean.
+      //
+      //     // Sinon c'est qu'on a passé un nom.
+      //     // Exécution d'une requête COUNT() avec une clause WHERE, et retourne un boolean.
+      //   }
+      //
+      //   public function get($info)
+      //   {
+      //     // Si le paramètre est un entier, on veut récupérer le personnage avec son identifiant.
+      //       // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet Personnage.
+      //
+      //     // Sinon, on veut récupérer le personnage avec son nom.
+      //     // Exécute une requête de type SELECT avec une clause WHERE, et retourne un objet Personnage.
+      //   }
+      //
+      //   public function getList($nom)
+      //   {
+      //     // Retourne la liste des personnages dont le nom n'est pas $nom.
+      //     // Le résultat sera un tableau d'instances de Personnage.
+      //   }
+      //
+      //   public function update(Personnage $perso)
+      //   {
+      //     // Prépare une requête de type UPDATE.
+      //     // Assignation des valeurs à la requête.
+      //     // Exécution de la requête.
+      //   }
+      // }
 
-        public function __construct($force, $experience){
-            echo 'voici le constructeur';
-            $this->setForce($force);
-            $this->setExperience($experience);
-            $this->setDegat(0);
-        }
 
-        public function force(){
-          return $this->_force;
-        }
-        public function experience(){
-          return $this->_experience;
-        }
-        public function degats(){
-          return $this->_degats;
-        }
-        public function setForce($force){
-          if (!is_int($force)) {
-            trigger_error('ça doit être un nombre entier',E_USER_WARNING);
-            return;
-          }
-          if ($force > 100) {
-            trigger_error('ça doit être plus petit que cent',E_USER_WARNING);
-            return;
-          }
-          $this->_force = $force;
-        }
+      //cours
 
-        public function setDegat($degat){
-          if (!is_int($degat)) {
-            trigger_error('ça doit être un nombre entier',E_USER_WARNING);
-            return;
-          }
-          if ($degat > 100) {
-            trigger_error('ça doit être plus petit que cent',E_USER_WARNING);
-            return;
-          }
-          $this->_degats = $degat;
-        }
+// class PersonnageManager{
+//   private $_bdd;
+//
+//   //récupère la base de donnee
+//   public function __construct($bdd){
+//     $this->setBdd($bdd);
+//   }
+//
+//   //remplit la propriété de l'objet pour constructeur
+//   public function setBdd($bdd){
+//     $this->_bdd = $bdd;
+//   }
+//   //ajoute un perso
+//   public function addPerso(Personnage $perso){
+//     $ajoutPerso = $this->_bdd->prepare('INSERT INTO Personnages(nom, forcePerso, degats, niveau, experience) values (:nom, :force, :degats, :niveau, :experience)');
+//     $ajoutPerso->bindValue(':nom', $perso->nom());
+//     $ajoutPerso->bindValue(':force', $perso->force(), PDO::PARAM_INT);
+//     $ajoutPerso->bindValue(':degats', $perso->degats(), PDO::PARAM_INT);
+//     $ajoutPerso->bindValue(':niveau', $perso->niveau(), PDO::PARAM_INT);
+//     $ajoutPerso->bindValue(':experience', $perso->experience(), PDO::PARAM_INT);
+//
+//     $ajoutPerso->execute();
+//
+//   }
+//   //supprime un perso
+//   public function deletePerso(Personnage $perso){
+//     $this->_bdd->execute('DELETE FROM Personnages WHERE id = '.$perso->id());
+//   }
+//   //recupere un perso
+//   public function getPerso($id){
+//     $id = (int)$id;
+//     $perso = $this->_bdd->execute('SELECT * FROM Personnages WHERE id = '.$perso->id());
+//     $donnees = $perso->fetch(PDO::FETCH_ASSOC);
+//
+//     return new Personnage($donnees);
+//   }
+//
+//   //met a jour un perso
+//   public function updatePerso(Personnage $perso){
+//     $updatePerso = $this->_db->prepare('UPDATE personnages SET forcePerso = :forcePerso, degats = :degats, niveau = :niveau, experience = :experience WHERE id = :id');
+//
+//     $updatePerso->bindValue(':forcePerso', $perso->forcePerso(), PDO::PARAM_INT);
+//     $updatePerso->bindValue(':degats', $perso->degats(), PDO::PARAM_INT);
+//     $updatePerso->bindValue(':niveau', $perso->niveau(), PDO::PARAM_INT);
+//     $updatePerso->bindValue(':experience', $perso->experience(), PDO::PARAM_INT);
+//     $updatePerso->bindValue(':id', $perso->id(), PDO::PARAM_INT);
+//
+//     $updatePerso->execute();
+//   }
+//   //recupere la liste de tout les perso
+//   public function getListPerso(){
+//     $persos = [];
+//
+//     $listPerso = $this->_db->query('SELECT id, nom, forcePerso, degats, niveau, experience FROM personnages ORDER BY nom');
+//
+//     while ($donnees = $listPerso->fetch(PDO::FETCH_ASSOC))
+//     {
+//       $persos[] = new Personnage($donnees);
+//     }
+//
+//     return $persos;
+//   }
+// }
+//
+//
+// //////////////////////PERSONNAGE///////
+//
+//
+// class Personnage{
+//   private $_id;
+//   private $_nom;
+//   private $_force;
+//   private $_degats;
+//   private $_niveau;
+//   private $_experience;
+//
+//   public function __construct(array $donnees){
+//     $this->hydrate($donnees);
+//   }
+//
+//   // Un tableau de données doit être passé à la fonction (d'où le préfixe « array »).
+//   public function hydrate(array $donnees){
+//   foreach ($donnees as $key => $value)
+//   {
+//     $method = 'set'.ucfirst($key);
+//
+//     if (method_exists($this, $method))
+//     {
+//       var_dump($method);
+//       $this->$method($value);
+//     }
+//   }
+// }
+//
+//   public function id() { return $this->_id; }
+//   public function nom() { return $this->_nom; }
+//   public function force() { return $this->_force; }
+//   public function degats() { return $this->_degats; }
+//   public function niveau() { return $this->_niveau; }
+//   public function experience() { return $this->_experience; }
+//
+//   public function setId($id)
+//   {
+//     // L'identifiant du personnage sera, quoi qu'il arrive, un nombre entier.
+//     $this->_id = (int) $id;
+//   }
+//
+//   public function setNom($nom)
+//   {
+//     // On vérifie qu'il s'agit bien d'une chaîne de caractères.
+//     // Dont la longueur est inférieure à 30 caractères.
+//     if (is_string($nom) && strlen($nom) <= 30)
+//     {
+//       $this->_nom = $nom;
+//     }
+//   }
+//
+//   public function setForce($force)
+//   {
+//     $force = (int) $force;
+//
+//     // On vérifie que la force passée est comprise entre 0 et 100.
+//     if ($force >= 0 && $force <= 100)
+//     {
+//       $this->_force = $force;
+//     }
+//   }
+//
+//   public function setDegats($degats)
+//   {
+//     $degats = (int) $degats;
+//
+//     // On vérifie que les dégâts passés sont compris entre 0 et 100.
+//     if ($degats >= 0 && $degats <= 100)
+//     {
+//       $this->_degats = $degats;
+//     }
+//   }
+//
+//   public function setNiveau($niveau)
+//   {
+//     $niveau = (int) $niveau;
+//
+//     // On vérifie que le niveau n'est pas négatif.
+//     if ($niveau >= 0)
+//     {
+//       $this->_niveau = $niveau;
+//     }
+//   }
+//
+//   public function setExperience($exp)
+//   {
+//     $exp = (int) $exp;
+//
+//     // On vérifie que l'expérience est comprise entre 0 et 100.
+//     if ($exp >= 0 && $exp <= 100)
+//     {
+//       $this->_experience = $exp;
+//     }
+//   }
+// }
+//
+// $perso = new Personnage([
+//   'nom' => 'theo',
+//   'force' => 12,
+//   'degats' => 3,
+//   'niveau' => 2,
+//   'experience' => 5
+// ]);
+//
+// var_dump($perso);
+// $bdd = new PDO('mysql:host=localhost;dbname=exoPoo', 'root', 'root');
+// $manager = new PersonnageManager($bdd);
+// //$manager->addPerso($perso);
+// var_dump($manager);
 
-        public function setExperience($experience){
-          if (!is_int($experience)) {
-            trigger_error('ça doit être un nombre entier',E_USER_WARNING);
-            return;
-          }
-          if ($experience > 100) {
-            trigger_error('ça doit être plus petit que cent',E_USER_WARNING);
-            return;
-          }
-          $this->_experience = $experience;
-        }
-
-        public function parler(){
-          echo 'bonjour';
-        }
-        public function deplacer(){
-
-        }
-        public function frapper(Personnage $persofrapper){
-          $persofrapper->_degats = $this->_force;
-          echo $persofrapper->_degats. 'points de dégâts';
-        }
-        public function gagnerExperience(){
-          $this->_experience = $this->_experience + 1;
-        }
-        public function afficheExperience(){
-          echo ' le perso a gagné : '. $this->_experience. ' d\'exp. ';
-        }
-      }
-      $perso1 = new Personnage(20,12);
-      $perso2 = new Personnage(32,24);
-
-      echo 'Le personnage 1 a ', $perso1->force(), ' de force, contrairement au personnage 2 qui a ', $perso2->force(), ' de force.<br />';
-echo 'Le personnage 1 a ', $perso1->experience(), ' d\'expérience, contrairement au personnage 2 qui a ', $perso2->experience(), ' d\'expérience.<br />';
-echo 'Le personnage 1 a ', $perso1->degats(), ' de dégâts, contrairement au personnage 2 qui a ', $perso2->degats(), ' de dégâts.<br />';
-
-
-// ORIENTE OBJET EXERCICE
+// ORIENTE OBJET EXERCICE 1 et 2
 // function chargerClasse($classe)
 // {
 //   require $classe . '.php'; // On inclut la classe correspondante au paramètre passé.
@@ -106,6 +342,85 @@ echo 'Le personnage 1 a ', $perso1->degats(), ' de dégâts, contrairement au pe
 //         spl_autoload_register('chargerClasse');
 //         $Ville = new Ville('bruxelle','Belgique');
 //         $Ville->afficherVille();
+
+// ORIENTE OBJET EXERCICE 3
+// class Personne{
+//   private $_nom;
+//   private $_prenom;
+//   private $_adresse;
+//
+//   public function __construct(array $donnees){
+//       $this->hydrate($donnees);
+//     }
+//
+//   public function hydrate(array $donnees){
+//     foreach ($donnees as $key => $value)
+//     {
+//       $method = 'set'.ucfirst($key);
+//
+//       if (method_exists($this, $method))
+//       {
+//         $this->$method($value);
+//       }
+//     }
+//   }
+//
+//   public function setNom($nom){
+//     $this->_nom = $nom;
+//   }
+//
+//   public function setPrenom($prenom){
+//     $this->_prenom = $prenom;
+//   }
+//
+//   public function setAdresse($adresse){
+//     $this->_adresse = $adresse;
+//   }
+//
+//   public function nom(){
+//     return $this->_nom;
+//   }
+//
+//   public function prenom(){
+//     return $this->_prenom;
+//   }
+//
+//   public function adresse(){
+//     return $this->_adresse;
+//   }
+//   public function getPersonne(){
+//     echo 'il est '.$this->prenom(). ' ' .$this->nom().' de '.$this->adresse();
+//   }
+// }
+//
+// $personne = new Personne([
+//   'nom' => 'silverberg',
+//   'prenom' => 'theo',
+//   'adresse' => 'castelblanc'
+// ]);
+// var_dump($personne);
+// $personne->setAdresse('Kirov');
+// $personne->getPersonne();
+
+//EXO OBJET 4
+
+class Form{
+  private $_text;
+  private $_submit;
+
+  public function __construct(){
+
+  }
+
+  public function setText($input){
+    $this->_text += $text;
+  }
+  public function setSubmit($submit){
+    $this->_submit = $submit;
+  }
+
+}
+
 ?>
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
